bb2f5d65a2676001e52fc4b3d782283d160afca9
Juan Bustamante <juanpineyrob@gmail.com>
2026-05-17 18:22:54 -0300

    chore: initial Spring Boot 4.0.6 scaffold
    Generated by Spring Initializr with Java 25, Spring Web MVC,
    Spring Data JPA, PostgreSQL driver, Lombok and DevTools.

---
+++ src/main/java/com/academconnect/AcademconnectApplication.java
@@ -0,0 +1,13 @@
+package com.academconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academconnect/AcademconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
