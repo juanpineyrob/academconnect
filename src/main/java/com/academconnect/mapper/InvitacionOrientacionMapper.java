6eb47040cf4c7d685cc31c3b5dfbe48707bc2a3d
Ignacio <juanpineyrob@gmail.com>
2026-06-06 20:59:04 -0300

    feat(camino-2.1): MapStruct InvitacionOrientacionMapper

---
+++ src/main/java/com/academconnect/mapper/InvitacionOrientacionMapper.java
@@ -0,0 +1,16 @@
+package com.academconnect.mapper;
+
+import com.academconnect.domain.InvitacionOrientacion;
+import com.academconnect.dto.InvitacionOrientacionResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface InvitacionOrientacionMapper {
+
+    @Mapping(source = "trabajo.id", target = "trabajoId")
+    @Mapping(source = "trabajo.titulo", target = "trabajoTitulo")
+    @Mapping(source = "profesor.id", target = "profesorId")
+    @Mapping(source = "profesor.nombre", target = "profesorNombre")
+    InvitacionOrientacionResponse toResponse(InvitacionOrientacion entity);
+}
